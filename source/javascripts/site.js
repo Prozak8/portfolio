18422683f46fa84acdb1a715e420d8c8b4e53799
Zak <zakary.howard94@gmail.com>
2018-11-27 14:48:04 +0100

    added on click functionality to project icons

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+function openModal(index) {
+    let modal = document.getElementById(`myModal_${index}`)
+    let span = document.getElementsByClassName("close")[index];   
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
