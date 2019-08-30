f921465ebe128e395c4219f3080d2dd350e423f8
Zak <zakary.howard94@gmail.com>
2019-08-30 14:54:53 +0200

    /about lorem placeholder, a link placeholders

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about__inner">
+      <h1>Zakary Howard</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio illo, maxime sed quia laboriosam nisi impedit delectus animi praesentium unde!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime culpa, ipsum facilis asperiores nostrum provident fugit nemo alias exercitationem, nisi molestias quos quis officia nulla magnam obcaecati aspernatur, quasi doloribus!</p>
+    </div>
+    <div class="social">
+      <ul>
+        <li>
+          <a href>Github</a>
+        </li>
+        <li>
+          <a href>LinkedIn</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<style lang="sass" scoped>
+@import '@/styles/variables.sass'
+.about
+  background-color: $white
+  height: 100%
+  width: 100%
+  &__inner
+    padding: 20px
+    h1, p
+      margin-bottom: 1rem
+  .social
+    margin-top: auto
+
+</style>
