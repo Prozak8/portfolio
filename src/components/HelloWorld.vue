67f18751f7f2dac4e7344d94f76c5093b37c89bc
Zak <zakary.howard94@gmail.com>
2019-08-30 14:42:07 +0200

    rework with vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="sass">
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {};
+  },
+  props: {}
+};
+</script>
+
+<style scoped lang="sass">
+@import '@/styles/variables.sass'
+
+.nav 
+  width: calc(100% - 60px)
+  height: calc(100% - 60px)
+  padding: 30px
+  background-color: $black
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  a 
+    color: $blue_light
+    padding: 1rem 2rem
+    margin-bottom: 1rem
+    text-decoration: none
+    background-color: $blue_dark
+    border-radius: 2rem
+    &.router-link-exact-active 
+      background-color: $blue_light
+      color: $blue_dark
+</style>
