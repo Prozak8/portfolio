8e75ab104388a0a576b46357768f2d35d27d1dbd
Zak <zakary.howard94@gmail.com>
2019-09-02 15:25:16 +0200

    added project cards. imported images.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,6 +2,7 @@
   <div class="nav">
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
+    <router-link to="/projects">Projects</router-link>
   </div>
 </template>
 
---
+++ src/views/Projects.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .container
+    .card
+      img(:src="plumcareLogo")
+    .card
+      img(:src="spideradsLogo")
+</template>
+
+<script>
+import plumcareLogo from "@/assets/plumcare-logo.png";
+import spideradsLogo from "@/assets/spiderads-logo.svg";
+
+export default {
+  data() {
+    return {
+      plumcareLogo: plumcareLogo,
+      spideradsLogo: spideradsLogo
+    };
+  },
+  components: {}
+};
+</script>
+<style lang="sass" scoped>
+@import '@/styles/variables.sass'
+
+.card
+  width: 90%
+  margin: auto
+  border: solid 1px $black
+  border-radius: 5px
+  &:nth-child(1)
+    margin-bottom: 2rem
+  
+  img
+    height: 4rem
+
+  &__header
+    font-size: 30px
+</style>
